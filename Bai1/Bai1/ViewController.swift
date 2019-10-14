87be1eaebf93c159cbe6488ce47dad2cb67938d3
MacOne <mac@MacOnes-MacBook-Pro.local>
2019-10-14 20:04:06 +0700

    bai tap 1

---
+++ Bai1/Bai1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bai1
+//
+//  Created by MacOne on 10/14/19.
+//  Copyright © 2019 MacOne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
